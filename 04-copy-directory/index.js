3205f92a4fb66e025a54d8ec28fc3af53e4c2046
Marina Frolova <frolova-marina37@mail.ru>
2023-05-09 05:04:02 +0300

    feat: 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs');
+
+const path = require('path');
+
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  fs.readdir(path.join(__dirname, 'files'), 
+    { withFileTypes: true },
+    (err, files) => {
+        if (err) {
+            console.log(err);
+          }
+
+      fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, err => {
+        if (err) {
+            console.log(err);
+          }
+        files.forEach(file => {
+
+          fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+            if (err) {
+                console.log(err);
+              }
+          });
+        });
+      }); 
+    }
+  );
+});
+
 
